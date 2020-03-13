3ce4fb4c8147162184f4b46baad4edabf99b23db
AndreVTavares <andretavares3@gmail.com>
2020-03-13 15:57:29 -0300

    creating dev model and connecting data with githubAPI

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express")
 const axios = require("axios")
+const Dev = require("./models/Dev")
 
 const routes = Router()
 
@@ -8,12 +9,23 @@ routes.get("/", (req, res) => {
 })
 
 routes.post("/devs", async (req, res) => {
-    const { github_username } = req.body
+    const { github_username, techs } = req.body
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
     const {name = login, bio, avatar_url} = apiResponse.data
 
+    const techsArray = techs.split(',').map(tech => tech.trim())
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        bio,
+        avatar_url,
+        techs: techsArray
+    })
+
+    return res.json(dev)
     
 })
 
