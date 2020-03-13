cecb60ce28db6438b5da9dece91f236a3eb8d675
AndreVTavares <andretavares3@gmail.com>
2020-03-13 14:39:01 -0300

    creating Devs model and using axios for getting data from github

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,20 @@
 const { Router } = require("express")
+const axios = require("axios")
 
 const routes = Router()
 
 routes.get("/", (req, res) => {
     return res.json({ message: 'Semana Omnistack!' })
+})
+
+routes.post("/devs", async (req, res) => {
+    const { github_username } = req.body
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+
+    const {name = login, bio, avatar_url} = apiResponse.data
+
+    
 })
 
 module.exports = routes
