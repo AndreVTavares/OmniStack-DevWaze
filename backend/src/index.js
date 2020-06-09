f8bdb27d02de21a51c8c4f9129f7e9897b15b0ca
AndreVTavares <andretavares3@gmail.com>
2020-06-09 14:36:39 -0300

    Installing CORS(backend) and fetching api data and showing on frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://andre:andre@restfulapi-k8xmw.mongodb.net/DevWaze
     useUnifiedTopology: true,
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
