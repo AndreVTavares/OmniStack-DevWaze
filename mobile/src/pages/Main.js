57b7aaf355e99cda269d05d81b7d3bcec0905588
AndreVTavares <andretavares3@gmail.com>
2020-06-11 12:18:21 -0300

    Creating Styles for main page

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import mapView, { Marker, Callout } from 'react-native-maps';
-import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons'
 
 const Main = ({ navigation }) => {
 
@@ -36,21 +37,35 @@ const Main = ({ navigation }) => {
 
 
     return (
-        <mapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={currentRegion}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/31252029?s=460&v=4' }} />
+        <>
+            <mapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={currentRegion}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/31252029?s=460&v=4' }} />
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'AndreVTavares' })
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>André Tavares</Text>
-                        <Text style={styles.devBio}>Tentando programar desde que me formei.</Text>
-                        <Text style={styles.devTechs}>ReactJS, Node.js, React Native</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </mapView>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'AndreVTavares' })
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>André Tavares</Text>
+                            <Text style={styles.devBio}>Tentando programar desde que me formei.</Text>
+                            <Text style={styles.devTechs}>ReactJS, Node.js, React Native</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </mapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..." 
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+                <TouchableOpacity onPress={() ={}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     )
 }
 
@@ -84,6 +99,42 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     },
 
 })
